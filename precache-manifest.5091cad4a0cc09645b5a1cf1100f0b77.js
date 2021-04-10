2d6efbb269e5216897e9150ace030665add87656
IhoraDev <Ihor.Lukachuk.Dev@gmail.com>
2021-04-10 12:28:26 +0300

    Updates

---
+++ precache-manifest.5091cad4a0cc09645b5a1cf1100f0b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4647f44785e0e962993e423ddb8d5339",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "2125188050421641b8e0",
+    "url": "/e-commerce/static/js/2.a03bf710.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/e-commerce/static/js/2.a03bf710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70d443f53cf7cc5ac24",
+    "url": "/e-commerce/static/js/main.9b2741c1.chunk.js"
+  },
+  {
+    "revision": "ca2aad11deeab9bfa755",
+    "url": "/e-commerce/static/js/runtime-main.ff030dd5.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/e-commerce/static/media/commerce.422fec10.png"
+  }
+]);
